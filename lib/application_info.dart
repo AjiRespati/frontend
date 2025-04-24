901fc1a79faef4959f66809fd924cdefd3791aaf
Aji Respati <aji@voltras.co.id>
2025-04-24 13:11:48 +0700

    persiapan 1.0.15

--- lib/application_info.dart
+++ lib/application_info.dart
@@ -18,7 +18,7 @@ class ApplicationInfo {
 
   static const isProduction = baseUrl == baseUrlProd;
 
-  static const appVersion = '1.0.13 ${isProduction ? "" : "DEV"}';
+  static const appVersion = '1.0.15 ${isProduction ? "" : "DEV"}';
 
   static const appName = "Gracia Rafflesia";
 
